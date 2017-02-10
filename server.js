dc625858fdf1ee05ec3fb9c1d520f4686806df17
Mathieu Ezannic <mezannic@gmail.com>
2017-02-10 18:03:57 +0000

    Take a date or UNIX timestamp as input and return both in a JSON object

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send(String(Date.now()))
+app.get('/:date?', function (req, res) {
+  var date, ret
+  
+  if (/^\d+$/.test(req.params.date)) {
+    // unix timestamp
+    date = new Date(req.params.date * 1000)
+  } else {
+    // natural language date
+    date = new Date(req.params.date)
+  }
+  
+  if (isNaN(date.getTime())) {
+    ret = {
+      unix: null,
+      natural: null
+    }
+  } else {
+    ret = {
+      unix: Math.round(date.getTime() / 1000),
+      natural: date.toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  }
+  
+  res.setHeader('Content-type', 'application/json; charset=utf-8')
+  res.json(ret)
 })
 
 app.listen(process.env.PORT, function () {
